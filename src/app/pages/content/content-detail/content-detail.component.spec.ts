c7e4ea6fd1b756b7889b05329560ee04c2fac3b4
Stijn van Houwelingen <stijn@van-houwelingen.net>
2021-11-29 10:49:26 +0100

    fixed detail test

--- src/app/pages/content/content-detail/content-detail.component.spec.ts
+++ src/app/pages/content/content-detail/content-detail.component.spec.ts
@@ -1,60 +1,70 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ActivatedRoute, convertToParamMap, Router, RouterModule } from '@angular/router';
-import {  of, Subscription} from 'rxjs';
+import {
+  ActivatedRoute,
+  convertToParamMap,
+  Router,
+  RouterModule,
+} from '@angular/router';
+import { of, Subscription } from 'rxjs';
 import { ContentService } from '../content.service';
 import { ContentInterface, ContentType } from '../content.model';
 import { ContentDetailComponent } from './content-detail.component';
-import { Directive, Input } from '@angular/core';
+import { Directive, HostListener, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-@Directive({selector: '[routerLink]',})
-export class RouterLinkStubDirective{
-  @Input('routerLink') linkParams: any; navigatedTo: any = null;
-  
+@Directive({ selector: '[routerLink]' })
+export class RouterLinkStubDirective {
+  @Input('routerLink') linkParams: any;
+  navigatedTo: any = null;
+  @HostListener('click')
+  onClick(): void {
+    this.navigatedTo = this.linkParams;
+  }
 }
 describe('ContentDetailComponent', () => {
   let component: ContentDetailComponent;
   let fixture: ComponentFixture<ContentDetailComponent>;
-  let contentService : any;
-  let contentServiceSpy : any;
-  let routerSpy : any;
-  beforeEach(async () => {
-    contentServiceSpy = jasmine.createSpyObj('ContentService',['getForId'])
-    routerSpy = jasmine.createSpyObj('Router',['navigateByUrl']);
-    await TestBed.configureTestingModule({
-      declarations: [ ContentDetailComponent,RouterLinkStubDirective ],
-      providers: [{provide: ContentService, useValue: contentServiceSpy},
-        {provide: ActivatedRoute,useValue: {
-            paramMap: of(convertToParamMap({id: 1}))
-        }
-      },
-    {provide: Router,useValue: routerSpy}],
-    imports:[RouterModule]
-    })
-    .compileComponents();
+  let contentServiceSpy: any;
+
+  beforeEach(() => {
+    contentServiceSpy = jasmine.createSpyObj('ContentService', ['getForId']);
+    TestBed.configureTestingModule({
+      declarations: [ContentDetailComponent, RouterLinkStubDirective],
+      providers: [
+        { provide: ContentService, useValue: contentServiceSpy },
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            paramMap: of(convertToParamMap({ id: 1 })),
+          },
+        },
+      ],
+      imports: [],
+    }).compileComponents();
     fixture = TestBed.createComponent(ContentDetailComponent);
     component = fixture.componentInstance;
   });
 
-  beforeEach( () => {
-    contentServiceSpy = jasmine.createSpyObj('ContentService',['getForId'])
-    contentServiceSpy.getForId.and.returnValue(of({id: 1, name: "Spain Revealed",
-    tags: ["spain", "culture","madrid"],
-    inProduction: true,
-    platforms: [{name: "Youtube",link: "https://www.youtube.com/c/JamesBlick/"}],
-    contentInterface: ContentInterface.Video,
-    contentType: ContentType.Videos,
-    language: "English",
-    targetLanguage: "Spanish"
-  }));
-    fixture = TestBed.createComponent(ContentDetailComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  afterAll(() => {
+    fixture.destroy();
   });
   //TODO fix this
-  xit('should create', () => {
-  // contentService.getForId(1).subscribe((item : any)=> console.log(item))
-  fixture.detectChanges();
-  expect(component).toBeTruthy();
+  fit('should create', () => {
+    const stubValue = {
+      id: 1,
+      name: 'Spain Revealed',
+      tags: ['spain', 'culture', 'madrid'],
+      inProduction: true,
+      platforms: [
+        { name: 'Youtube', link: 'https://www.youtube.com/c/JamesBlick/' },
+      ],
+      contentInterface: ContentInterface.Video,
+      contentType: ContentType.Videos,
+      language: 'English',
+      targetLanguage: 'Spanish',
+    };
+    contentServiceSpy.getForId.and.returnValue(of(stubValue));
+    fixture.detectChanges();
+    expect(component).toBeTruthy();
   });
 });
